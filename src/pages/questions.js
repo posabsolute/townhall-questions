f4755c1d0344fc25ec2609b2b3452099dabc7cc6
Cedric Dugas <cedric.dugas@gmail.com>
2017-02-09 16:27:08 -0500

    add question button

--- src/pages/questions.js
+++ src/pages/questions.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import base from '../configs/firebase';
 import Question from '../components/question';
 import AppBar from 'material-ui/AppBar';
-
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ContentAdd from 'material-ui/svg-icons/content/add';
+import './questions.css';
 
 class Questions extends Component {
   constructor(props) {
@@ -23,13 +25,18 @@ class Questions extends Component {
   render() {
     return (
       <div className="questions">
-        <AppBar title="Town Hall Live Questions" style={{background:'#333739 '}} />
+        <AppBar title="Town Hall Live Questions" style={{background:'#333739'}} />
         {this.state.questions.sort((a, b) => a.votes < b.votes).map((item, index) => {
           return <Question
             name={item.name}
             description={item.description}
             votes={item.votes} />
         })}
+        <div className="button">
+        	<FloatingActionButton backgroundColor='#333739'>
+    	      <ContentAdd />
+    	    </FloatingActionButton>
+    	</div>
       </div>
     );
   }
