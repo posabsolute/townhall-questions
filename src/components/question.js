076a9657fcfbada034af5ffa294c3b1afd357793
Taylor Brennan <taylor.brennan@appdirect.com>
2017-02-10 14:21:27 -0800

    redo vote toggle

--- src/components/question.js
+++ src/components/question.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import base from '../configs/firebase';
 import Badge from 'material-ui/Badge';
-import {ImageExposurePlus1, ImageExposureNeg1} from 'material-ui/svg-icons';
+import {ContentAdd, ContentRemove} from 'material-ui/svg-icons';
 import FloatingActionButton from 'material-ui/FloatingActionButton';
 import {Card, CardActions, CardHeader} from 'material-ui/Card';
 
@@ -12,7 +12,7 @@ class Question extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      voted: !!(this.props.voters && this.props.voters[this.props.user.uid])
     };
   }
 
@@ -21,6 +21,7 @@ class Question extends Component {
     var voters = this.props.voters || {};
     var vote = voters[this.props.user.uid] ? -1 : 1;
     voters[this.props.user.uid] = !voters[this.props.user.uid];
+    this.setState({voted: voters[this.props.user.uid]});
 
     base.update(`questions/${this.props.uuid}`, {
       data: {
@@ -45,12 +46,19 @@ class Question extends Component {
             <Badge
               badgeContent={this.props.votes}
               primary={true}
+              badgeStyle={{
+                'background-color': this.state.voted ? "#76BD22" : "#E33D51"
+              }}
             />
           }
         />
         <CardActions>
-          <FloatingActionButton className="vote-button" onTouchTap={this.handleVote.bind(this)}>
-            <ImageExposurePlus1 />
+          <FloatingActionButton
+            className="vote-button"
+            onTouchTap={this.handleVote.bind(this)}
+            backgroundColor="#009ABF"
+          >
+            {this.state.voted ? <ContentRemove /> : <ContentAdd />}
           </FloatingActionButton>
         </CardActions>
       </Card>
