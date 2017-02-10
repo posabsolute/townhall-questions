5f3f76f21b761425d2edb5d3d94a140fe9c616ed
Taylor Brennan <taylor.brennan@appdirect.com>
2017-02-10 13:09:02 -0800

    toggle votes

--- src/components/question.js
+++ src/components/question.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import base from '../configs/firebase';
 import Badge from 'material-ui/Badge';
-import PlusOneIcon from 'material-ui/svg-icons/social/plus-one';
+import {ImageExposurePlus1, ImageExposureNeg1} from 'material-ui/svg-icons';
 import FloatingActionButton from 'material-ui/FloatingActionButton';
 import {Card, CardActions, CardHeader} from 'material-ui/Card';
 
@@ -9,10 +9,24 @@ import {Card, CardActions, CardHeader} from 'material-ui/Card';
 
 class Question extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
   handleVote = (e) => {
   	e.preventDefault();
+    var voters = this.props.voters || {};
+    var vote = voters[this.props.user.uid] ? -1 : 1;
+    voters[this.props.user.uid] = !voters[this.props.user.uid];
+
     base.update(`questions/${this.props.uuid}`, {
-      data: {votes: this.props.votes + 1},
+      data: {
+        votes: this.props.votes + vote,
+        voters: voters
+      },
       then(err){
         if(err){
           console.error(err);
@@ -36,7 +50,7 @@ class Question extends Component {
         />
         <CardActions>
           <FloatingActionButton className="vote-button" onTouchTap={this.handleVote.bind(this)}>
-            <PlusOneIcon />
+            <ImageExposurePlus1 />
           </FloatingActionButton>
         </CardActions>
       </Card>
--- src/pages/questions.js
+++ src/pages/questions.js
@@ -19,60 +19,67 @@ class Questions extends Component {
     base.bindToState(`questions`, {
       context: this,
       state: 'questions',
-      asArray: true
+      asArray: true,
+      queries: {
+        orderByChild: 'votes'
+      }
     });
   }
 
   saveQuestion() {
-  	var uuid = generateGuid();
-	if (this.state.question) {
-		this.handleClose();
-	  	base.post(`questions/${uuid}`, {
-	    	data: {
-		    	name: this.state.question, 
-		    	description: this.state.description,
-		    	user: this.props.user.uid,
-		    	uuid: uuid,
-		    	votes: 0,
-		    	townhall: this.props.currentTownhall.key,
-		    	key: uuid,
-		    }
-	    });
-	    
-	}
+    var uuid = generateGuid();
+    var voters = {};
+    voters[this.props.user.uid] = true;
+    if (this.state.question) {
+      this.handleClose();
+      base.post(`questions/${uuid}`, {
+        data: {
+          name: this.state.question,
+          description: this.state.description,
+          user: this.props.user.uid,
+          uuid: uuid,
+          votes: 1,
+          voters: voters,
+          townhall: this.props.currentTownhall.key,
+          key: uuid,
+        }
+      });
+    }
 
-  	function generateGuid() {
-	  var result, i, j;
-	  result = '';
-	  for(j=0; j<32; j++) {
-	    if( j == 8 || j == 12|| j == 16|| j == 20) 
-	      result = result + '-';
-	    i = Math.floor(Math.random()*16).toString(16).toUpperCase();
-	    result = result + i;
-	  }
-	  return result;
-  	}
+    function generateGuid() {
+      var result, i, j;
+      result = '';
+      for(j=0; j<32; j++) {
+        if( j == 8 || j == 12|| j == 16|| j == 20)
+          result = result + '-';
+        i = Math.floor(Math.random()*16).toString(16).toUpperCase();
+        result = result + i;
+      }
+      return result;
+    }
   }
 
   render() {
-  	var questions = this.state.questions ? this.state.questions.filter(question => parseInt(question.townhall, 10) === parseInt(this.props.currentTownhall.key, 10)) : undefined;
+    var questions = this.state.questions ? this.state.questions.filter(question => parseInt(question.townhall, 10) === parseInt(this.props.currentTownhall.key, 10)) : undefined;
     return (
       <div className="questions-page">
         <AppBar className="appBar" title="Town Hall Live Questions" style={{background:'#333739'}} />
         <div className='questions'>
           <FlipMove>
-          {questions && questions.sort((a, b) => a.votes < b.votes).map((item, index) => {
+          {questions && questions.reverse().map((item, index) => {
             return <Question
               name={item.name}
               description={item.description}
               votes={item.votes}
+              voters={item.voters}
               uuid={item.uuid}
-              key={item.key} />
+              key={item.key}
+              user={this.props.user} />
           })}
           </FlipMove>
           {questions && !questions.length ? (<div className="question">There is currently no questions, be the first!</div>) : null }
         </div>
-		<ModalAddQuestion user={this.props.user} currentTownhall={this.props.currentTownhall} submit={this.saveQuestion} open={this.state.openModal} />
+        <ModalAddQuestion user={this.props.user} currentTownhall={this.props.currentTownhall} submit={this.saveQuestion} open={this.state.openModal} />
       </div>
     );
   }
